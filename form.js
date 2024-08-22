15b91b24514ef21f313f8f61b8a22b441a48fd8c
Harshitajain18 <hjain0457@gmail.com>
2024-08-22 18:05:01 +0530

    initial commmit

---
+++ form.js
@@ -0,0 +1,18 @@
+let FirstName = document.querySelector("#fname")
+let LastName = document.querySelector("#lname")
+let Mail = document.querySelector("#mail")
+let Contact = document.querySelector("#phone")
+let Comment = document.querySelector("#comment")
+let CheckBox = document.querySelector("#check")
+let EmpStatus = document.querySelector("#status")
+let ColorOption = document.querySelector("#option")
+let Button = document.querySelector("#btn")
+let array =document.querySelectorAll(".all")
+array.forEach(element => {
+    console.log(element);
+    localStorage.getItem('items');
+});
+
+<input type = "text" onChange={()=>{element.target.value}}> </input>
+
+
